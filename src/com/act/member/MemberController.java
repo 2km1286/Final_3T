37310bee1f04c818f5d9132316a15ea510ab4df7
haaging <forvivahakyung@gmail.com>
2023-08-01 17:41:02 +0900

    [update] redirect로 수정 / 데이터insert결과 분기처리
    redirect로 해야 새로고침 시 데이터 연속입력되지 않음

--- src/com/act/member/MemberController.java
+++ src/com/act/member/MemberController.java
@@ -32,9 +32,14 @@ public class MemberController
 	{
 		String result = "";
 		System.out.println("확인1");
-		memberService.join(session, dto);
+		int num = memberService.join(session, dto);
+		if(num!=1)
+			result = "redirect:join.action";
+		else
+			result = "redirect:main.action"; 
+		
 		System.out.println("확인2");
-		result = "/WEB-INF/views/MainPage.jsp";
+		
 		return result;
 	}
 	
