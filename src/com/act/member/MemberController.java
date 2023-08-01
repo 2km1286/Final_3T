097bafd2741a978ad9548408ff356acd5ef4a6c9
haaging <forvivahakyung@gmail.com>
2023-08-01 15:05:43 +0900

    [update] MemberController.java 생성
    매퍼 컨트롤러

--- src/com/act/member/MemberController.java
+++ src/com/act/member/MemberController.java
@@ -1,13 +1,19 @@
 package com.act.member;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController
 {
+	@Autowired
+	private MemberService memberService;
+	
 	/*
 	@RequestMapping("/memberlogin.action")
 	public String print(HttpServletRequest request)
@@ -20,4 +26,23 @@ public class MemberController
 		return view;
 	}
 	*/
+	
+	@RequestMapping("/join.action")
+	public String join(HttpSession session, MemberDTO dto)
+	{
+		String result = "";
+		System.out.println("확인1");
+		//memberService.join(session, dto);
+		System.out.println("확인2");
+		result = "/WEB-INF/views/MainPage.jsp";
+		return result;
+	}
+	
+	@RequestMapping("/main.action")
+	public String main()
+	{
+		String result = "MenuForm3.jsp";
+		return result;
+	}
+	
 }
