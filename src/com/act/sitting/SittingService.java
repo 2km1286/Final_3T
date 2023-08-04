ae2f286caca683fc2d746591623ff9f4018f1c33
haaging <forvivahakyung@gmail.com>
2023-08-04 16:49:41 +0900

    [update] 컨트롤러 어노테이션 추가

--- src/com/act/sitting/SittingService.java
+++ src/com/act/sitting/SittingService.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SittingService
 {
 	@Autowired
