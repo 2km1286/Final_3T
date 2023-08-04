ea185197c30d1812f48e0cb07111b55f7ac9a7ad
haaging <forvivahakyung@gmail.com>
2023-08-04 14:26:58 +0900

    [create] 펫시팅의 DB관련 메소드를 담당하는 SittingService 매퍼 생성

---
+++ src/com/act/sitting/SittingService.java
@@ -0,0 +1,25 @@
+package com.act.sitting;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SittingService
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// 펫시터의 정보를 리스트로 가져오기
+	public ArrayList<SittingDTO> list()
+	{
+		ArrayList<SittingDTO> list = new ArrayList<SittingDTO>();
+		
+		ISittingDAO dao = sqlSession.getMapper(ISittingDAO.class);
+		
+		list = dao.list();
+		
+		return list;
+	}
+	
+}
