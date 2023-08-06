3c00396330dc797e14454767588a933c994eb9a6
haaging <123478243+haaging@users.noreply.github.com>
2023-08-06 22:16:59 +0900

    [update] 범례 태그 메소드 이름 변경 및 등급 메소드 추가
    tagList → IndexTagList (왜냐하면 해당 돌봄장소의 태그를 가져오는 메소드의 이름을 tagList 로 하기 위해서) / 등급 메소드 : gradeList

--- src/com/act/sitting/SittingService.java
+++ src/com/act/sitting/SittingService.java
@@ -24,14 +24,37 @@ public class SittingService
 	}
 	
 	// 펫시터 돌봄장소 범례태그 리스트 가져오기
-	public ArrayList<SittingDTO> tagList()
+	public ArrayList<SittingDTO> IndexTagList()
 	{
-		ArrayList<SittingDTO> tagList = new ArrayList<SittingDTO>();
+		ArrayList<SittingDTO> IndexTagList = new ArrayList<SittingDTO>();
 		
 		ISittingDAO dao = sqlSession.getMapper(ISittingDAO.class);
-		tagList = dao.tagList();
+		IndexTagList = dao.IndexTagList();
 		
-		return tagList;
+		return IndexTagList;
 	}
+	
+	// 펫시터의 등급과 기본가격 정보 
+	public ArrayList<SittingDTO> gradeList()
+	{
+		ArrayList<SittingDTO> grade = new ArrayList<SittingDTO>();
+		
+		ISittingDAO dao = sqlSession.getMapper(ISittingDAO.class);
+		grade = dao.gradeList();
+		
+		return grade;
+	}
+	
+	// 해당 돌봄장소의 태그들 가져오기
+	/*
+	 * public ArrayList<SittingDTO> tagList(SittingDTO s) { ArrayList<SittingDTO>
+	 * tagList = new ArrayList<SittingDTO>();
+	 * 
+	 * ISittingDAO dao = sqlSession.getMapper(ISittingDAO.class);
+	 * 
+	 * tagList = dao.tagList(s.getMemSid());
+	 * 
+	 * return tagList; }
+	 */
 	
 }
