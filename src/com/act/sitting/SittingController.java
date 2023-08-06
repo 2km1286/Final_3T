ad52caa56374f5a49c957c7ddaacfc41ad80c187
haaging <123478243+haaging@users.noreply.github.com>
2023-08-06 22:15:07 +0900

    [update] 범례태그 메소드 이름 변경 및 등급 메소드 추가

--- src/com/act/sitting/SittingController.java
+++ src/com/act/sitting/SittingController.java
@@ -1,7 +1,5 @@
 package com.act.sitting;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,16 +14,20 @@ public class SittingController
 
 	// 펫시팅(돌봄장소) 리스트업 페이지로 가기
 	@RequestMapping("/sittingList.action")
-	public String sittingList(Model model)
+	public String sittingList(Model model, SittingDTO s)
 	{
 		String view = "";
 		
-		//ArrayList<SittingDTO> list = zsittingService.list();
-
 		model.addAttribute("list", sittingService.list());
-		model.addAttribute("tagList", sittingService.tagList());
-		System.out.println("list: " + sittingService.list());
-		System.out.println("tagList: " + sittingService.tagList());
+		model.addAttribute("IndexTagList", sittingService.IndexTagList());
+		model.addAttribute("gradeList", sittingService.gradeList());
+		//model.addAttribute("tagList", sittingService.tagList(s));
+		
+		
+		//System.out.println("list: " + sittingService.list());
+		//System.out.println("tagList: " + sittingService.tagList());
+		//System.out.println("gradeList: " + sittingService.gradeList());
+		
 		view = "/WEB-INF/views/SittingListPage.jsp";
 		return view;
 	}
