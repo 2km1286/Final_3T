3c82ce678f0a0866a00d0c8ed3fce0f39c0769ee
haaging <forvivahakyung@gmail.com>
2023-08-07 12:43:27 +0900

    [update] 펫시팅 관련 액션4개 추가

--- src/com/act/sitting/SittingController.java
+++ src/com/act/sitting/SittingController.java
@@ -11,25 +11,64 @@ public class SittingController
 	@Autowired
 	private SittingService sittingService;
 
-
 	// 펫시팅(돌봄장소) 리스트업 페이지로 가기
 	@RequestMapping("/sittingList.action")
 	public String sittingList(Model model, SittingDTO s)
 	{
 		String view = "";
-		
+
 		model.addAttribute("list", sittingService.list());
 		model.addAttribute("IndexTagList", sittingService.IndexTagList());
 		model.addAttribute("gradeList", sittingService.gradeList());
-		//model.addAttribute("tagList", sittingService.tagList(s));
-		
-		
-		//System.out.println("list: " + sittingService.list());
-		//System.out.println("tagList: " + sittingService.tagList());
-		//System.out.println("gradeList: " + sittingService.gradeList());
-		
-		view = "/WEB-INF/views/SittingListPage.jsp";
+		// model.addAttribute("tagList", sittingService.tagList(s));
+
+		// System.out.println("list: " + sittingService.list());
+		// System.out.println("tagList: " + sittingService.tagList());
+		// System.out.println("gradeList: " + sittingService.gradeList());
+
+		view = "/WEB-INF/views/sitting/SittingListPage.jsp";
 		return view;
+	}
+
+	// 펫시터 예약화면
+	@RequestMapping("/sitting-reservation.action")
+	public String getSittingReservationView()
+	{
+		String result = "";
+
+		result = "/WEB-INF/views/sitting/SittingReservationPage.jsp";
+
+		return result;
+	}
+
+	// 마이페이지 펫시팅의 수정하기를 눌렀을 때, AJAX처리
+	@RequestMapping("/updateSPInfoForm.action")
+	public String updateSPInfoForm()
+	{
+		String result = "";
+		// AJAX이자 컴포넌트
+		result = "/WEB-INF/components/updateSPInfoForm.jsp";
+		return result;
+	}
+
+	// 마이페이지 펫시팅의 예약내역을 눌렀을 때, AJAX처리
+	@RequestMapping("/sittingBookList.action")
+	public String sittingBookList()
+	{
+		String result = "";
+		// AJAX이자 컴포넌트
+		result = "/WEB-INF/components/sittingBookList.jsp";
+		return result;
+	}
+
+	// 마이페이지 펫시팅의 후기내역을 눌렀을 때, AJAX처리
+	@RequestMapping("/sittingReviewList.action")
+	public String sittingReviewList()
+	{
+		String result = "";
+		// AJAX이자 컴포넌트
+		result = "/WEB-INF/components/sittingReviewList.jsp";
+		return result;
 	}
 
 }
