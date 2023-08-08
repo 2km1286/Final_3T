28c6fc254fc1e691b51e44924f4c25839ac63067
기배 <ki_bae@naver.com>
2023-08-08 19:52:07 +0900

    [UPDATE] IWalkDAO에 list()와 Tag() 가져오는 메소드 추가

--- src/com/act/walk/IWalkDAO.java
+++ src/com/act/walk/IWalkDAO.java
@@ -1,6 +1,12 @@
 package com.act.walk;
 
+import java.util.ArrayList;
+
+import com.act.walk.WalkDTO;
+
 public interface IWalkDAO
 {
+	public ArrayList<WalkDTO> list();
+	public ArrayList<WalkDTO> IndexTagList();
 
 }
