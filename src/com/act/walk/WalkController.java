b635e9ccb74946ab6c89559bfa6874bbef447447
기배 <ki_bae@naver.com>
2023-08-08 19:51:19 +0900

    [UPDATE] WalkController action 추가

--- src/com/act/walk/WalkController.java
+++ src/com/act/walk/WalkController.java
@@ -4,27 +4,34 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.act.walk.WalkDTO;
+
 @Controller
-public class WalkController
-{
+public class WalkController {
+
 	@Autowired
-	private IWalkService walkService;
-	
-	// 대리산책 리스트업 페이지로 가기
+	private WalkService walkService;
+
+	// 대리산책 공고글 리스트업 페이지로 가기
 	@RequestMapping("/walklistpage.action")
-	public String petList()
+	public String walkList(Model model, WalkDTO s) 
 	{
+
 		String view = "";
+
+		model.addAttribute("list", walkService.list());
+		model.addAttribute("IndexTagList", walkService.IndexTagList());
+
 		view = "/WEB-INF/views/walk/WalkListPage.jsp";
 		return view;
 	}
-	
+
 	// 대리산책러 예약화면
 	@RequestMapping("/walkreservationpage.action")
-	public String getReservationView()
-	{
+	public String getReservationView() {
 		String result = "";
 
 		result = "/WEB-INF/views/walk/WalkReservationPage.jsp";
@@ -34,24 +41,31 @@ public class WalkController
 
 	// 대리산책러 결제화면
 	@RequestMapping("/reservationpaymentpage.action")
-	public String getReservationPaymentView()
-	{
+	public String getReservationPaymentView() {
 		String result = "";
 
 		result = "/WEB-INF/views/index/ReservationPaymentPage.jsp";
 
 		return result;
 	}
-	
+
 	// 마이페이지 대리산책. AJAX로 처리.
 	@RequestMapping("/mypagewalkform.action")
-	public String myPageWalk()
-	{
+	public String myPageWalk() {
 		String result = "";
 		// AJAX이자 컴포넌트
 		result = "/WEB-INF/ajax/MyPageWalkForm.jsp";
 		return result;
 	}
 	
-	
+	// 대리산책러 자격 시험 시작
+	@RequestMapping("/walktest.action")
+	public String walkTest() {
+		String result = "";
+
+		result = "/WEB-INF/views/walk/WalkTestStartPage.jsp";
+
+		return result;
+	}
+
 }
