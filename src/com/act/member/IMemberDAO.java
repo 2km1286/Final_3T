31fdf75b9a8ee9559963b8169998998cb8fb83ff
haaging <123478243+haaging@users.noreply.github.com>
2023-08-09 00:44:54 +0900

    [update] sitting패키지로 이동

--- src/com/act/member/IMemberDAO.java
+++ src/com/act/member/IMemberDAO.java
@@ -33,10 +33,4 @@ public interface IMemberDAO
 	// 닉네임 중복 확인
 	public int searchNick(String jmNickName);
 	
-	// 회원의 시험제출 유무 확인
-	public int stsCount(MemberDTO dto);
-	
-	// 회원의 펫시터 면허번호 유무 확인
-	public int slCount(MemberDTO dto);
-	
 }
