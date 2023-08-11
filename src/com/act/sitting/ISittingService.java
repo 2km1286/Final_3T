2fbefaf600d8c45562873a41a8a90884d023737d
haaging <forvivahakyung@gmail.com>
2023-08-11 17:52:10 +0900

    [update] 태그검색 시 태그리스트 조회 메소드 진행중 및 gradeList메소드 삭제

--- src/com/act/sitting/ISittingService.java
+++ src/com/act/sitting/ISittingService.java
@@ -1,13 +1,14 @@
 package com.act.sitting;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ISittingService
 {
 	public ArrayList<SittingDTO> list();
 	public ArrayList<SittingDTO> IndexTagList();
-	//public ArrayList<SittingDTO> gradeList(SittingDTO dto);
 	public ArrayList<SittingDTO> tagList();
 	public ArrayList<SittingDTO> booklist(String memSid);
 	public ArrayList<SittingDTO> sittingFilterList(SittingDTO dto);
+	public ArrayList<SittingDTO> sittingFilterTagList(SittingDTO dto);
 }
