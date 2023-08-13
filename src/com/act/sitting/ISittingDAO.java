7919000a2fc035485c1b75637ba3c80ffc5c7aff
haaging <123478243+haaging@users.noreply.github.com>
2023-08-14 03:09:14 +0900

    [update] 펫시팅 예약화면 바인딩 - 메소드 선언 및 주석

--- src/com/act/sitting/ISittingDAO.java
+++ src/com/act/sitting/ISittingDAO.java
@@ -7,8 +7,13 @@ import com.act.walk.WalkDTO;
 
 public interface ISittingDAO
 {
+	// 공개 상태인 돌봄장소들의 정보 조회
 	public ArrayList<SittingDTO> list();
+	
+	// 펫시팅 돌봄장소의 범례태그들 조회
 	public ArrayList<SittingDTO> IndexTagList();
+	
+	// 해당 돌봄장소의 태그들 확인
 	public ArrayList<SittingDTO> tagList();
 
 	// 회원의 시험제출 유무 확인
@@ -34,6 +39,9 @@ public interface ISittingDAO
 	// 회원번호로 예전~현재 돌봄장소 기본정보 조회
 	public ArrayList<SittingDTO> sittingPlaceBasic(String memSid);
 	
+	// 돌봄공간 번호로 예약페이지에 들어갈 정보 조회
+	public SittingDTO listBySpSid(int spSid);
+	
 	// ※현재 사용하고 있는 돌봄장소※의 태그들 조회
 	public ArrayList<SittingDTO> sittingPlaceTags(int spSid);
 	
@@ -43,8 +51,10 @@ public interface ISittingDAO
 	// 나에게 달린 후기를 쓴 회원번호로 그 회원의 닉네임 조회를 위한 전체출력
 	public ArrayList<SittingDTO> sittingReviewers();
 	
+	// 펫시팅 리스트 검색 1차(지역/견수)
 	public ArrayList<SittingDTO> spfilterlist(SittingDTO dto);
 	
+	// 펫시팅 리스트 검색 2차(태그)
 	public ArrayList<SittingDTO> spFilterTagList(SittingDTO dto);
 	
 	// 견주로서 오늘 펫시팅 예약 취소된건
@@ -64,5 +74,8 @@ public interface ISittingDAO
 	
 	// SRWSID 로 후기 한 건 조회
 	public SittingDTO sittingReview(int srwSid);
+	
+	// 공개상태인 돌봄장소의 휴일 조회
+	public ArrayList<SittingDTO> spRest(int spSid);
 
 }
