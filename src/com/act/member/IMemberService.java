3efd7b992a1a28a72d433e9e1e710217ca3719f6
기배 <ki_bae@naver.com>
2023-08-16 07:57:54 +0900

    [UPDATE] IMemberService의 넘겨주는 값 수정

--- src/com/act/member/IMemberService.java
+++ src/com/act/member/IMemberService.java
@@ -9,10 +9,10 @@ public interface IMemberService
 	public String searchMemsid(MemberDTO dto);
 	
 	// 아이디 비밀번호로 펫시터 타임라인 종료 횟수 찾기
-	public String searchSittingcount(MemberDTO dto);
+	public int searchSittingcount(String memSid);
 	
 	// 아이디 비밀번호로 대리산책 타임라인 종료 횟수 찾기
-	public String searchWalkcount(MemberDTO dto);
+	public int searchWalkcount(String memSid);
 	
 	public int searchId(String jmId);
 	
